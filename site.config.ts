526210bb552ae5bff5ac4ab4a633c5687a9be75a
ictcentervnn <admin@ictcenter.tk>
2022-08-16 14:51:22 +0700

    First commit

---
+++ site.config.ts
@@ -0,0 +1,64 @@
+import { siteConfig } from './lib/site-config'
+
+export default siteConfig({
+  // the site's root Notion page (required)
+  rootNotionPageId: '10f45248f91944a38e9016cac597b87b',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'ICTcenter - Blog Chia Sẻ Kiến Thức Tin Học',
+  domain: 'ictcenter.xyz',
+  author: 'ictcenter',
+
+  // open graph metadata (optional)
+  description: 'ICTcenter - Blog Chia Sẻ Kiến Thức Tin Học',
+
+  // social usernames (optional)
+  twitter: '#',
+  github: 'ictcentervnn',
+  //linkedin: '#',
+  newsletter: '#', // optional newsletter URL
+  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  isPreviewImageSupportEnabled: true,
+
+  // whether or not redis is enabled for caching generated preview images (optional)
+  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+  // environment variables. see the readme for more info
+  isRedisEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null,
+
+  // whether to use the default notion navigation style or a custom one with links to
+  // important pages
+  //navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+       title: 'About',
+       pageId: '60df2c2640884d768b6c9be7393add3b'
+     },
+     {
+       title: 'Contact',
+       pageId: '549e9e8fe4124561b3424d0522ca4645'
+     }
+   ]
+})
