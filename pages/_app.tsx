c74841c6286d04388442faaafeac6e73b6d11429
ictcentervnn <admin@ictcenter.tk>
2023-04-25 11:13:34 +0700

    edit app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -69,16 +69,3 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return <Component {...pageProps} />
 }
-//import type { AppProps } from 'next/app';
-import { Analytics } from '@vercel/analytics/react';
- 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  );
-}
- 
-export default MyApp;
