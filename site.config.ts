d98ba7b895a43995d6c2ca8dfd6032391ec6ab55
ictcentervnn <admin@ictcenter.tk>
2023-04-25 15:54:52 +0700

    update sit.config

--- site.config.ts
+++ site.config.ts
@@ -50,8 +50,8 @@ export default siteConfig({
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
   //navigationStyle: 'default'
-   navigationStyle: 'custom',
-   navigationLinks: [
+  navigationStyle: 'custom',
+  navigationLinks: [
      {
        title: 'About',
        pageId: 'f1199d37579b41cbabfc0b5174f4256a'
